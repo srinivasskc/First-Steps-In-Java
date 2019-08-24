b057cf3ad66108a374b474a69dc3a59933ea74bc
srinivas kadiyala <srinivas.skc30@gmail.com>
2019-08-24 18:07:10 +0530

    Update to Test Project

--- Test/src/com/learning/app/App.java
+++ Test/src/com/learning/app/App.java
@@ -1,8 +1,26 @@
+//Package Name - Lower Case Letters
+
 package com.learning.app;
 
-public class App {
-	public static void main(String[] args) {
+
+//Public - Access Modifier: Outside of the class, Class - Keyword, App - Class Name.
+
+public class App 
+{ //Opening of the Class
+
+	//Public - Access Modifier; 
+	//Static is Java Keyword - Main Method is Static and We can call it directly.
+	//No need to create an object of the Class to call this method
+	// Main - By default method, called at starting directly from JVM.
+	//Void - No return type. If return type, we can change to String/int
+	//String[] args - command line arguments.
+	
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello world Java Learning from Eclipse");
+		
+		System.out.print("This is my first Java Program...");
+		System.out.print("Happy Learning!");
 	}
 
-}
+} //Closing of the Class
